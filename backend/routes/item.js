0d149b16f686b8f6eb0126f3c0975798da09294d
panupong mikada <path301242@gmail.com>
2022-02-21 14:49:50 +0700

    updata api

--- backend/routes/item.js
+++ backend/routes/item.js
@@ -4,11 +4,40 @@ const Item = require('../model/items')
 
 router.get('/', (req, res) => {
     Item.find((err, data) => {
-        if (err) {
-            return console.log(err)
-        }else {
-            res.json(data)
-        }
+        if (err) return console.log(err)
+        res.json(data)
+    })
+})
+
+router.post('/insert', (req, res) => {
+    const data = new Item( req.body )
+    data.save((err, data) => {
+        if (err) return console.log(err)
+        res.json(data)
+        console.log("insert success!!")
+    })
+})
+
+router.get('/edit/:id', (req, res) => {
+    Item.findOne({_id: req.params.id}, (err, data) => {
+        if(err) console.log(err)
+        res.json(data)
+        console.log("show edit success!!")
+    })
+})
+
+router.put('/updata/:id', (req, res) => {
+    Item.findByIdAndUpdate(req.params.id, req.body, (err, data) => {
+        if(err) console.log(err)
+        res.json(data)
+        console.log("updata success!!")
+    })
+})
+
+router.delete('/delete/:id', (req,res) => {
+    Item.findByIdAndDelete(req.params.id, (err, data) =>{
+        if(err) console.log(err)
+        console.log("delete success!!")
     })
 })
 
