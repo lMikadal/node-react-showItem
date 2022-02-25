4f109119cb26610815c3654d0322481e420bd237
panupong mikada <path301242@gmail.com>
2022-02-25 16:28:26 +0700

    update card

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,15 @@
 import Content from './componets/Content';
 import Navbar from './componets/Navbar';
 import './index.css';
+import "@material-tailwind/react/tailwind.css";
 
 function App() {
     return (
         <div>
             <Navbar />
-            <Content />
+            <div className='max-w-7xl mx-auto px-4 py-4'>
+                <Content />
+            </div>
         </div>
     );
 }
--- frontend/src/componets/Content.js
+++ frontend/src/componets/Content.js
@@ -1,7 +1,21 @@
+import Card from "@material-tailwind/react/Card";
+import CardBody from "@material-tailwind/react/CardBody";
+import Paragraph from "@material-tailwind/react/Paragraph";
+
 function Content() {
     return (
-      <>
-      </>  
+        <div className="w-1/5">
+            <Card>
+                <CardBody>
+                    <h1 color="gray">Card Title</h1>
+                    <Paragraph color="gray">
+                        Don't be scared of the truth because we need to restart the human
+                        foundation in truth And I love you like Kanye loves Kanye I love
+                        Rick Owens’ bed design but the back is...
+                    </Paragraph>
+                </CardBody>
+            </Card>  
+        </div>
     );
 }
 
